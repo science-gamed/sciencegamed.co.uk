ffc00c355a16ff401220e6572c1fce1c54b79cf2
Rich Harris <richard.a.harris@gmail.com>
2013-03-09 16:47:14 +0000

    added watch task dependency

--- appfog/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-app = express.createServer();
-/*app.get('/', function(req, res) {
-    res.send('testing');
-});*/
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.VCAP_APP_PORT || 3000);
